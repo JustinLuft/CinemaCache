76863db5306b72d229825d78d3dd17f59512e7e6
Justin <89544777+JustinLuft@users.noreply.github.com>
2025-06-03 14:56:58 -0400

    Create index.ts

---
+++ Root Directory/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as LoginPage } from './LoginPage';
+export { default as SkillsPage } from './SkillsPage';
+export { default as AboutPage } from './AboutPage';
+export { default as ProjectShowcasePage } from './ProjectShowcasePage';
